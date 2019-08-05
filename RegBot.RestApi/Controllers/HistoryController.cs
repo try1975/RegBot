4d4eb03af7fa8462accd445c16a4723dceeb5f54
try1975 <try1975@mail.ru>
2019-08-05 22:11:01 +0300

    add history to rest api

---
+++ RegBot.RestApi/Controllers/HistoryController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Common.Service.Interfaces;
+using LiteDB;
+using log4net;
+
+namespace RegBot.RestApi.Controllers
+{
+    public class HistoryController : ApiController
+    {
+        private static readonly ILog Log = LogManager.GetLogger(nameof(EmailController));
+        private static readonly string AppPath = HttpRuntime.BinDirectory; //HttpRuntime.AppDomainAppPath
+        private readonly string connectionString;
+
+        public HistoryController()
+        {
+            connectionString = Path.Combine(AppPath, ConfigurationManager.AppSettings["DbPath"]);
+        }
+
+        [HttpGet]
+        [Route("History")]
+        [ResponseType(typeof(IEnumerable<IAccountData>))]
+        public async Task<IHttpActionResult> GetHistory()
+        {
+            using (var db = new LiteDatabase(connectionString))
+            {
+                return Ok(db.GetCollection<IAccountData>("AccountsData").FindAll().OrderByDescending(z => z.Id));
+            }
+        }
+    }
+}
